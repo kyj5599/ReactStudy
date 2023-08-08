62a8e6cd7e2b8dbf0bf6a245fb8a6d13c51b4624
kyj5599 <277k@naver.com>
2023-08-08 17:57:29 +0900

    useRef(DOM)_로그인

--- useref/src/App.js
+++ useref/src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const App = () => {
-  const [count, setCount] = useState(1);
-  const renderCount = useRef(1);
+  const inputRef = useRef()
 
   useEffect(() => {
-    renderCount.current = renderCount.current + 1;
-    console.log('렌더링 수: ', renderCount.current);
-  });
+    // console.log(inputRef);
+    inputRef.current.focus();
+  }, []);
 
+  const login = () => {
+    alert(`환영합니다 ${inputRef.current.value}!`)
+    inputRef.current.focus();
+  }
 
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>올려</button>
+      <input ref={inputRef} type="text" placeholder="username"></input>
+      <button onClick={login}>로그인</button>
     </div>
   );
 }
