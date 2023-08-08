eef5c61c5bd50c5607c920ff33730a4bdeed2c02
kyj5599 <277k@naver.com>
2023-08-08 14:34:25 +0900

    react basic(CRUD) 복습

--- test/src/App.js
+++ test/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'
+import { useState } from 'react';
 
 // READ
 // 컴포넌트
-// Prop(속성) => 입력 -> 컴포넌트를 사용하는 외부자를 위한 데이터
+// props(속성) => 입력 -> 컴포넌트를 사용하는 *외부자를 위한 데이터
 // return => 출력
 // CREATE
-// State -> (Prop와다른점)컴포넌트를 만드는 내부자를 위한 데이터
+// State -> (Prop와다른점)컴포넌트를 만드는 *내부자를 위한 데이터
 // useState(PRIMITIVE) -> string, number, bigint, boolean, undefinde, symbol, null
 // useState(Object) -> object, array
 // UPDATE = CREATE + READ
@@ -53,17 +53,17 @@ function Create(props){
       const title = event.target.title.value;
       const body = event.target.body.value;
       props.onCreate(title, body);
-
     }}>
       <p><input type="text" name="title" placeholder="title"/></p>
-      <p><textarea name="body" placeholder='body'/></p>
+      <p><textarea name="body" placeholder='body'></textarea></p>
       <p><input type='submit' value="Create"></input></p>
     </form>
   </article>
 }
 function Update(props){
+  // state로 환승(setTitle, setBody)
   const [title, setTitle] = useState(props.title);
-  const [body, setBody] = useState(props.title);
+  const [body, setBody] = useState(props.body);
   return <article>
     <h2>Update</h2>
     <form onSubmit={event=>{
@@ -73,20 +73,20 @@ function Update(props){
       props.onUpdate(title, body);
 
     }}>
+      {/* onChange => 값을 입력할때마다 호출됨 */}
       <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{
         // console.log(event.target.value);
         setTitle(event.target.value);
       }}/></p>
       <p><textarea name="body" placeholder='body' value={body} onChange={event=>{
-        // console.log(event.target.value);
         setBody(event.target.value);
       }}/></p>
       <p><input type='submit' value="Update"></input></p>
     </form>
   </article>
 }
-
 function App() {
+  // useState
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
   const [nextId, setNextId] = useState(4);
@@ -108,12 +108,14 @@ function App() {
         body = topics[i].body;
       }
     }
+    // content = <Article title="Read" body="Hello, Read"></Article>
     content = <Article title={title} body={body}></Article>
     contextControl = <>
       <li><a href={'/update/'+id} onClick={event=>{
         event.preventDefault();
         setMode('UPDATE');
       }}>Update</a></li>
+      {/* Delete */}
       <li><input type="button" value="Delete" onClick={()=>{
         const newTopics = []
         for(let i=0; i<topics.length; i++){
@@ -128,9 +130,11 @@ function App() {
   } else if(mode === 'CREATE'){
     content = <Create onCreate={(_title, _body)=>{
       const newTopic = {id:nextId, title:_title, body:_body}
+      // 렌더링
       const newTopics = [...topics]
       newTopics.push(newTopic);
       setTopics(newTopics);
+      //
       setMode('READ');
       setId(nextId);
       setNextId(nextId+1);
@@ -144,7 +148,7 @@ function App() {
       }
     }
     content = <Update title={title} body={body} onUpdate={(title, body)=>{
-      console.log(title, body);
+      // console.log(title, body);
       const newTopics = [...topics]
       const updatedTopic = {id:id, title:title, body:body}
       for(let i=0; i<newTopics.length; i++){
@@ -167,7 +171,8 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
-      <ul>
+      {/* <ul></ul> 태그로 목록화 */}
+      <ul> 
         <li><a href="/create" onClick={event=>{
           event.preventDefault();
           setMode('CREATE');
