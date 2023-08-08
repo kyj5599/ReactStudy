ffcd26072da5dc31930990fb2184079e69f908ff
kyj5599 <277k@naver.com>
2023-08-08 16:38:57 +0900

    useEffect(ClenUp)_Timer예제

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,44 +1,14 @@
 // import './App.css';
 import { useState, useEffect } from 'react';
+import Timer from './component/Timer';
 
 function App() {
-  const [count, setCount] = useState(1);
-  const [name, setName] = useState('');
-
-  const handleCountUpdate = () => {
-    setCount(count + 1);
-  };
-
-  const handleInputChange = (e) => {
-    setName(e.target.value);
-  }; 
-
-  // 렌더링마다 매번 실행됨 - 렌더링 이후
-  useEffect(() => {
-    console.log('렌더링 ✨');
-  })
-
-  // 마운팅 + count가 변경될때만 실행
-  useEffect(()=>{
-    console.log('🐱 count 변화')
-  }, [count]);
-
-  // 마운팅 + name이 변경될때만 실행
-  useEffect(()=>{
-    console.log('🐱‍👤 name 변화')
-  }, [name]);
-
-  useEffect(() => {
-    console.log('마운팅 👏')
-  }, [])
-
+  const [showTimer, setShowTimer] = useState(false);
   return (
     <div>
-      <button onClick={handleCountUpdate}>Update</button>
-      <span>count: {count} </span>
-      <br></br>
-      <input type='text' value={name} onChange={handleInputChange}></input>
-      <span>name : {name}</span>
+      {/* showTimer가 True일때만 Timer가 돌아가게끔 */}
+      {showTimer && <Timer />}
+      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
     </div>
   );
 }
